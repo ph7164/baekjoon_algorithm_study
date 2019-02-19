93de8d2ef17446babf997a3f4666a103290eb2a2
philhwa <ph71640@naver.com>
2019-02-20 01:33:35 +0900

    first

---
+++ level1.cpp
@@ -0,0 +1,105 @@
+#include<stdio.h>
+
+
+// 2557
+#if 0
+
+int main() {
+	printf("Hello World!");
+
+	return 0;
+
+}
+
+#endif
+
+// 1000
+#if 0
+
+int main(void) {
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a + b);
+
+	return 0;
+}
+
+#endif
+
+// 1001
+#if 0
+
+int main(void) {
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a - b);
+
+	return 0;
+}
+
+#endif
+
+// 7287
+#if 0
+
+int main(void) {
+	printf("1");
+	printf("ph7164");
+
+	return 0;
+}
+
+#endif
+
+// 10172
+#if 0
+
+int main(void) {
+
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+	return 0;
+
+}
+
+#endif
+
+// 10718
+#if 0
+
+int main() {
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군\n");
+
+	return 0;
+}
+
+#endif
+
+// 11718, 11719
+#if 1
+
+int main(void) {
+	
+	int a;
+	while (1) {
+		a = getchar();
+
+		if (a == EOF) {
+			break;
+		}
+		putchar(a);
+
+	}
+	return 0;
+}
+
+#endif
+
